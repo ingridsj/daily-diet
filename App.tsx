0ea2fb80753e3c682ec8be57da03bb9b0685b6f3
ingridsj <ingridsjalmeida@gmail.com>
2023-06-10 13:22:10 -0300

    criando tipo do tema e usando theme provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "@theme/index";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    </ThemeProvider>
   );
 }
