fd573f47a1de4f89b160060448809841d4433991
ingridsj <ingridsjalmeida@gmail.com>
2023-06-10 13:11:14 -0300

    definindo tema

---
+++ src/theme/index.tsx
@@ -0,0 +1,34 @@
+export default {
+  COLORS: {
+    RED_DARK: "#BF3B44",
+    RED_MID: "#F3BABD",
+    RED_LIGHT: "#F4E6E7",
+
+    GREEN_DARK: "#639339",
+    GREEN_MID: "#CBE4B4",
+    GREEN_LIGHT: "#E5F0DB",
+
+    GRAY_100: "#1B1D1E",
+    GRAY_200: "#333638",
+    GRAY_300: "#5C6265",
+    GRAY_400: "#B9BBBC",
+    GRAY_500: "#DDDEDF",
+    GRAY_600: "#EFF0F0",
+    GRAY_700: "#FAFAFA",
+
+    WHITE: "#FFFFFF",
+  },
+  FONT_FAMILY: {
+    REGULAR: "NunitoSans_400Regular",
+    BOLD: "NunitoSans_700Bold",
+    BLACK: "NunitoSans_900Black",
+  },
+  FONT_SIZE: {
+    XXSM: 12,
+    XSM: 14,
+    SM: 16,
+    MD: 18,
+    LG: 24,
+    XL: 32,
+  },
+};
