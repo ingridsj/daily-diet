f3f9b8cd0ff717011ffa4ed482fd5190b4aaec08
ingridsj <ingridsjalmeida@gmail.com>
2023-06-10 13:33:54 -0300

    instalando fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "@theme/index";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  NunitoSans_900Black,
+} from "@expo-google-fonts/nunito-sans";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+    NunitoSans_900Black,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <View>
-        <Text>Hello world!</Text>
+        {fontsLoaded ? <Text>Hello world!</Text> : <ActivityIndicator />}
       </View>
     </ThemeProvider>
   );
