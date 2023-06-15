f46cb1473a96957814b97edeca8e14fd7709c339
ingridsj <ingridsjalmeida@gmail.com>
2023-06-14 21:34:57 -0300

    finalizando estilização página home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,140 @@
-import { Image } from "react-native";
+import { Image, TouchableOpacity, SectionList, View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Plus } from "phosphor-react-native";
+import { useTheme } from "styled-components";
+import { StatisticsCard } from "@components/StatisticsCard";
+import { TextValue } from "@components/TextValue";
+import { Button } from "@components/Button";
+import { MealCard } from "@components/MealCard";
+import theme from "@theme/index";
 
 import logoImg from "@assets/logo.png";
 import avatar from "@assets/avatar.jpg";
 
 import * as S from "./styles";
 
+const DATA = [
+  {
+    date: "12/06/2023",
+    data: [
+      {
+        hour: "10:00",
+        name: "Pizza",
+        description: "Pizza de Calabresa",
+        inDiet: "outDiet",
+      },
+      {
+        hour: "11:00",
+        name: "Couve",
+        description: "Pizza de Calabresa",
+        inDiet: "inDiet",
+      },
+    ],
+  },
+  // {
+  //   date: "11/06/2023",
+  //   data: [
+  //     {
+  //       hour: "10:00",
+  //       name: "Pizza",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //     {
+  //       hour: "11:00",
+  //       name: "Danone",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "inDiet",
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: "12/06/2023",
+  //   data: [
+  //     {
+  //       hour: "10:00",
+  //       name: "Pizza",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //     {
+  //       hour: "11:00",
+  //       name: "Pizza",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //   ],
+  // },
+  // {
+  //   date: "12/06/2023",
+  //   data: [
+  //     {
+  //       hour: "10:00",
+  //       name: "Pizza",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //     {
+  //       hour: "11:00",
+  //       name: "Pizza",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //     {
+  //       hour: "13:00",
+  //       name: "Chocolate",
+  //       description: "Pizza de Calabresa",
+  //       inDiet: "outDiet",
+  //     },
+  //   ],
+  // },
+];
+
 export function Home() {
+  const { FONT_SIZE } = useTheme();
+
+  const navigation = useNavigation();
+
+  function handleOpenMeal() {
+    navigation.navigate("statistics");
+  }
+
   return (
     <S.Container>
       <S.Header>
         <Image source={logoImg} />
         <S.Avatar source={avatar} />
       </S.Header>
+      <TouchableOpacity onPress={handleOpenMeal}>
+        <StatisticsCard showOpenButton height={102} variant="inDiet">
+          <TextValue
+            title="99%"
+            subtitle="das refeições dentro da dieta"
+            fontSize={FONT_SIZE.XL}
+          />
+        </StatisticsCard>
+      </TouchableOpacity>
+      <S.NewMealWrapper>
+        <S.MealTitle>Refeições</S.MealTitle>
+        <Button
+          title="Nova Refeição"
+          icon={<Plus color={theme.COLORS.WHITE} />}
+        />
+      </S.NewMealWrapper>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item.description + index}
+        renderSectionHeader={({ section: { date } }) => (
+          <View>
+            <S.MealListTitle>{date}</S.MealListTitle>
+          </View>
+        )}
+        renderItem={({ item }) => (
+          <MealCard hour={item.hour} name={item.name} variant={item.inDiet} />
+        )}
+        fadingEdgeLength={200}
+        showsVerticalScrollIndicator={false}
+      />
     </S.Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Container = styled(SafeAreaView)`
@@ -11,6 +11,7 @@ export const Header = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  margin-bottom: 40px;
 `;
 
 export const Avatar = styled.Image`
@@ -19,4 +20,27 @@ export const Avatar = styled.Image`
   border-radius: 50px;
   border-width: 2px;
   border-color: ${({ theme }) => theme.COLORS.GRAY_200};
+`;
+
+export const NewMealWrapper = styled.View`
+  margin-top: 36px;
+`;
+
+export const MealListTitle = styled.Text`
+  margin-top: 32px;
+  margin-bottom: 8px;
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_100};
+  `}
+`;
+
+export const MealTitle = styled.Text`
+  margin-bottom: 8px;
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `};
 `;
